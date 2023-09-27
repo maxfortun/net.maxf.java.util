21798dcdc1397e094b4cc596b93d1adfa537fc92
Max <max@maxf.net>
2023-09-27 16:59:21 -0400

    updated

--- src/test/java/net/maxf/java/util/ResourceBundleChainTest.java
+++ src/test/java/net/maxf/java/util/ResourceBundleChainTest.java
@@ -42,11 +42,30 @@ public class ResourceBundleChainTest {
 		}
 	}
 
-/*
 	@Test
 	public void testResourceBundleChainGet() throws Exception {
+		ResourceBundle envResourceBundle = new EnvResourceBundle();
+
+		Map<String, String> map = new HashMap<>();
+		map.put("mapKey1", "mapValue1");
+		map.put("mapKey2", "mapValue2");
+		ResourceBundle mapResourceBundle = new MapResourceBundle(map);
+
+		ResourceBundle emptyMapResourceBundle = new MapResourceBundle(new HashMap<String, String>());
+
+		ResourceBundle fileResourceBundle = ResourceBundle.getBundle("ApplicationResources");
+
+		List<ResourceBundle> list = Arrays.asList(new ResourceBundle[]{ envResourceBundle, mapResourceBundle, emptyMapResourceBundle, fileResourceBundle});
+
+		logger.fine("ResourceBundleChain of "+list.size()+" bundles");
+
+		ResourceBundle resourceBundle = new ResourceBundleChain(list);
+
+
+		assertTrue(resourceBundle.getString("env_bundle").equals("env"));
+		assertTrue(resourceBundle.getString("mapKey2").equals("mapValue2"));
+		assertTrue(resourceBundle.getString("key1").equals("file"));
 	}
-*/
 
 }
 
