c78e6b3d578a2be9c3153302cc967b533559d4d8
Max <max@maxf.net>
2023-09-27 16:16:56 -0400

    updated

---
+++ src/test/java/net/maxf/java/util/MapResourceBundleTest.java
@@ -0,0 +1,51 @@
+package net.maxf.java.util;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import net.maxf.java.util.EnvResourceBundle;
+
+public class MapResourceBundleTest {
+	private Logger logger = Logger.getLogger(MapResourceBundleTest.class.getName());
+
+	@Test
+	public void testMapResourceBundleKeys() throws Exception {
+		Map<String, String> map = new HashMap<>();
+		map.put("mapKey1", "value1");
+		map.put("mapKey2", "value2");
+
+		ResourceBundle resourceBundle = new MapResourceBundle(map);
+		Enumeration<String> keyEnumeration = resourceBundle.getKeys();
+		while(keyEnumeration.hasMoreElements()) {
+			String key = keyEnumeration.nextElement();
+			String value = resourceBundle.getString(key);
+			logger.fine(key+"="+value);
+		}
+
+	}
+
+	@Test
+	public void testMapResourceBundleChange() throws Exception {
+		Map<String, String> map = new HashMap<>();
+		map.put("mapKey1", "value1");
+		map.put("mapKey2", "value2");
+
+		ResourceBundle resourceBundle = new MapResourceBundle(map);
+		
+		map.put("mapKey2", "value2-changed");
+
+		String value = resourceBundle.getString("mapKey2");
+
+		assertTrue(value.equals("value2-changed"));
+
+	}
+
+}
+
